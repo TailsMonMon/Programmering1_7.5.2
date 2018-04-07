06e3076d6f2d50b4df5a7cca28efcd9af18d1ddf
Monica Björk <m.bjork95@gmail.com>
2018-04-07 20:09:45 +0200

    Add project files.

---
+++ Programmering1_7.5.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmering1_7._5._2 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Välj ett nummer mellan 1 och 100: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            
+            if(num>=1 && num<=100) {
+                do {
+                    Console.Write("{0} ", num++);
+                } while(num<=101);
+            }
+            else {
+                return;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
